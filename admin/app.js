34d5348d46768d2e957a889bc980990cde4c90ba
jinti <shenjindi@fourz.cn>
2023-06-04 14:52:54 +0800

    use Alpine.store

--- admin/app.js
+++ admin/app.js
@@ -1,43 +1,84 @@
+class Queryresult {
+    constructor() {
+        this.reset()
+    }
+    reset() {
+        this.pos = 0
+        this.total = 0
+        this.limit = 20
+        this.items = []
+        this.count = 0
+    }
+    attach(data, current) {
+        this.pos = data.pos || 0
+        this.total = data.total || 0
+        this.limit = data.limit || 20
+        let items = data.items || []
+        this.count = items.length
+        if (this.count && this.pos == 0) {
+            this.pos = 1
+        }
+
+        // build items for table view
+        this.items = items.map(item => {
+            let row = []
+            current.shows.forEach(field => {
+                row.push({
+                    value: item[field.name],
+                    primary: field.primary,
+                    selected: false,
+                })
+            })
+            return row
+        })
+    }
+}
+class AdminObject {
+    constructor(meta) {
+        this.meta = meta
+        this.name = meta.name
+        this.path = meta.path
+        this.group = meta.group
+        this.primaryKey = meta.primaryKey
+        this.pluralName = meta.pluralName
+        let shows = []
+        if (meta.shows) {
+            shows = meta.shows.map(f => {
+                return { name: f }
+            })
+        } else {
+            shows = meta.fields
+        }
+        this.shows = shows.map(f => {
+            return {
+                headerName: f.name.toUpperCase().replace(/_/g, ' '),
+                name: f.name,
+                primary: f.name === meta.primaryKey,
+            }
+        })
+    }
+    get showsearch() {
+        return (this.meta.searchables || []).length > 0
+    }
+}
+
 const adminapp = () => ({
     loading: true,
     switching: false,
     site: {},
-    objects: {},
-    current: null,
     navmenus: [],
-    qureyresult: {
-        pos: 0,
-        total: 0,
-        limit: 20,
-        items: [],
-        count: 0,
-    },
     async init() {
+        Alpine.store('queryresult', new Queryresult())
+        Alpine.store('current', {})
         this.$router.config({ mode: 'hash', base: '/admin/' })
         let resp = await fetch('./admin.json', {
             method: 'POST',
         })
         let meta = await resp.json()
         this.site = meta.site
-        this.objects = meta.objects
-        // convert objects.fileds.name to UPPER_CASE
-        this.objects.forEach(obj => {
-            if (!obj.shows) {
-                obj.shows = obj.fields
-            } else {
-                obj.shows = obj.shows.map(f => {
-                    return { name: f }
-                })
-            }
+        let objects = meta.objects.map(obj => new AdminObject(obj))
+        Alpine.store('objects', objects)
 
-            obj.shows = obj.shows.map(f => {
-                return {
-                    headerName: f.name.toUpperCase().replace(/_/g, ' '),
-                    name: f.name,
-                    primary: f.name === obj.primaryKey,
-                }
-            })
-        })
 
         this.user = meta.user
         this.user.name = this.user.firstName || this.user.email
@@ -45,7 +86,7 @@ const adminapp = () => ({
 
         if (this.$router.path) {
             // switch to current object
-            let obj = this.objects.find(obj => obj.path === this.$router.path)
+            let obj = this.$store.objects.find(obj => obj.path === this.$router.path)
             if (obj) {
                 this.switchobject(null, obj)
             }
@@ -55,7 +96,7 @@ const adminapp = () => ({
 
     build_navmenu() {
         let menus = []
-        this.objects.forEach(obj => {
+        this.$store.objects.forEach(obj => {
             let menu = menus.find(m => m.name === obj.group)
             if (!menu) {
                 menu = { name: obj.group, items: [] }
@@ -71,18 +112,18 @@ const adminapp = () => ({
             event.preventDefault()
         }
 
-        if (this.current) {
-            if (this.current === obj) return
-            this.current.active = false
+        if (this.$store.current) {
+            if (this.$store.current === obj) return
+            this.$store.current.active = false
         }
 
         let elm = document.getElementById('objectcontent')
         if (elm) elm.innerHTML = ''
 
-        this.qureyresult = {}
+        this.$store.queryresult.reset()
         this.switching = true
-        this.current = obj
-        this.current.active = true
+        this.$store.current = obj
+        this.$store.current.active = true
 
         this.$router.push(obj.path)
 
@@ -108,13 +149,13 @@ const adminapp = () => ({
     },
 
     selectAllResult(event) {
-        this.qureyresult.items.forEach(row => {
+        this.$store.queryresult.items.forEach(row => {
             row.selected = !row.selected
         })
     },
     refreshcurrent() {
         let query = {}
-        let path = this.current.path
+        let path = this.$store.current.path
         if (path[-1] != '/') {
             path += '/'
         }
@@ -123,28 +164,7 @@ const adminapp = () => ({
             body: JSON.stringify(query),
         }).then(resp => {
             resp.json().then(data => {
-
-                this.qureyresult.pos = data.pos || 0
-                this.qureyresult.total = data.total || 0
-                this.qureyresult.limit = data.limit || 20
-                let items = data.items || []
-                this.qureyresult.count = items.length
-                if (this.qureyresult.count && this.qureyresult.pos == 0) {
-                    this.qureyresult.pos = 1
-                }
-
-                // build items for table view
-                this.qureyresult.items = items.map(item => {
-                    let row = []
-                    this.current.shows.forEach(field => {
-                        row.push({
-                            value: item[field.name],
-                            primary: field.primary,
-                            selected: false,
-                        })
-                    })
-                    return row
-                })
+                this.$store.queryresult.attach(data, this.$store.current)
             })
         })
     },
